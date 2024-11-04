3d6a98a8ff9ecab4a8440b9fbf602b5edceddcc3
최미래 <miraechoi@dgsw.hs.kr>
2024-11-05 08:33:24 +0900

    (#1) feature :: add user error code

---
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/user/exception/UserErrorCode.kt
@@ -0,0 +1,16 @@
+package com.socksfactory.naeyangkku.domain.user.exception
+
+import com.socksfactory.naeyangkku.global.exception.CustomErrorCode
+import org.springframework.http.HttpStatus
+
+enum class UserErrorCode (
+    override val status: HttpStatus,
+    override val state: String,
+    override val message: String,
+) : CustomErrorCode {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND", "유저를 찾을 수 없습니다."),
+    USER_ALREADY_EXIST(HttpStatus.CONFLICT, "CONFLICT", "유저가(이메일이) 이미 존재합니다."),
+    USER_NOT_MATCH(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "이메일 또는 비밀번호가 잘못되었습니다.")
+
+}
