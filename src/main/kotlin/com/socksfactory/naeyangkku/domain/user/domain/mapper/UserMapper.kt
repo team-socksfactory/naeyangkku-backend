06ab8300b159063e1ba413e71e95a482209f21ee
최미래 <miraechoi@dgsw.hs.kr>
2024-11-05 08:32:44 +0900

    (#1) feature :: add user mapper

---
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/mapper/UserMapper.kt
@@ -0,0 +1,36 @@
+package com.socksfactory.naeyangkku.domain.user.domain.mapper
+
+import com.socksfactory.naeyangkku.domain.user.domain.entity.UserEntity
+import com.socksfactory.naeyangkku.domain.user.domain.model.User
+import com.socksfactory.naeyangkku.domain.user.presentation.dto.request.RegisterUserRequest
+import com.socksfactory.naeyangkku.global.common.Mapper
+import org.springframework.stereotype.Component
+
+@Component
+class UserMapper(
+): Mapper<User, UserEntity> {
+    override fun toDomain(entity: UserEntity): User {
+        return User(
+            id = entity.id,
+            email = entity.email,
+            name = entity.name,
+            password = entity.password
+        )
+    }
+
+    override fun toEntity(domain: User): UserEntity {
+        return UserEntity(
+            email = domain.email,
+            name = domain.name,
+            password = domain.password
+        )
+    }
+
+    fun toDomain(registerUserRequest: RegisterUserRequest, password: String): User {
+        return User(
+            email = registerUserRequest.email,
+            name = registerUserRequest.name,
+            password = password
+        )
+    }
+}
