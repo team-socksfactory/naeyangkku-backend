1c9aceaff2ba9162892ae2df75a44bdc83e31793
최미래 <miraechoi@dgsw.hs.kr>
2024-11-05 08:35:33 +0900

    (#1) feature :: add base response

---
+++ src/main/kotlin/com/socksfactory/naeyangkku/global/common/BaseResponse.kt
@@ -0,0 +1,15 @@
+package com.socksfactory.naeyangkku.global.common
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import org.springframework.http.HttpStatus
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class BaseResponse<T> (
+
+    val status: Int = HttpStatus.OK.value(),
+    val state: String? = "OK",
+    val message: String,
+    val data: T? = null
+
+)
+
