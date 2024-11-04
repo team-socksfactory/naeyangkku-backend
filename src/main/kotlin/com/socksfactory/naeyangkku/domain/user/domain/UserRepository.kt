dc417c1d0e0002c500e3dfcfbdae7e6fab081f63
최미래 <miraechoi@dgsw.hs.kr>
2024-11-05 08:32:49 +0900

    (#1) feature :: add user repository

---
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.socksfactory.naeyangkku.domain.user.domain
+
+import com.socksfactory.naeyangkku.domain.user.domain.entity.UserEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository : JpaRepository<UserEntity, Long> {
+    fun findByEmail(email: String): UserEntity?
+    fun existsByEmail(email: String): Boolean
+}
