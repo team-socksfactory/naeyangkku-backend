b7e69138eab427459550adef8f485eea480ed316
최미래 <miraechoi@dgsw.hs.kr>
2024-11-05 08:36:23 +0900

    (#1) feature :: jwt error exception and type

---
+++ src/main/kotlin/com/socksfactory/naeyangkku/global/auth/jwt/exception/JwtErrorCode.kt
@@ -0,0 +1,20 @@
+package com.socksfactory.naeyangkku.global.auth.jwt.exception
+
+import com.socksfactory.naeyangkku.global.exception.CustomErrorCode
+import org.springframework.http.HttpStatus
+
+enum class JwtErrorCode (
+    override val status: HttpStatus,
+    override val state: String,
+    override val message: String,
+): CustomErrorCode {
+
+    JWT_TOKEN_EXPIRED(HttpStatus.FORBIDDEN, "FORBIDDEN", "토큰이 만료 되었습니다."),
+    JWT_TOKEN_SIGNATURE_ERROR(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "토큰의 서명이 일치하지 않습니다."),
+    JWT_TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "토큰이 구조가 이상하거나 데이터가 일치하지 않습니다."),
+    JWT_TOKEN_UNSUPPORTED_ERROR(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "지원하지 않는 토큰입니다."),
+    JWT_TOKEN_ILL_EXCEPTION(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "JWT 처리 과정에서 오류가 발생했습니다."),
+    JWT_UNKNOWN_EXCEPTION(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "JWT 처리과정에서 알수없는 오류가 발생했습니다."),
+    JWT_EMPTY_EXCEPTION(HttpStatus.FORBIDDEN, "FORBIDDEN", "토큰을 넣어주세요."),
+
+}
---
+++ src/main/kotlin/com/socksfactory/naeyangkku/global/auth/jwt/exception/type/JwtErrorType.kt
@@ -0,0 +1,11 @@
+package com.socksfactory.naeyangkku.global.auth.jwt.exception.type
+
+enum class JwtErrorType {
+    OK,
+    ExpiredJwtException,
+    SignatureException,
+    MalformedJwtException,
+    UnsupportedJwtException,
+    IllegalArgumentException,
+    UNKNOWN_EXCEPTION
+}
