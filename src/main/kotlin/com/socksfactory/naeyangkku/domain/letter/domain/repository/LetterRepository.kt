b634a75deb19227827cb9c2a6074ac4c83082750
최미래 <miraechoi@dgsw.hs.kr>
2024-11-05 20:39:13 +0900

    (#2) refactor :: letter repository letter to letter entity

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/domain/repository/LetterRepository.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/domain/repository/LetterRepository.kt
@@ -1,9 +1,9 @@
 package com.socksfactory.naeyangkku.domain.letter.domain.repository
 
-import com.socksfactory.naeyangkku.domain.letter.domain.model.Letter
+import com.socksfactory.naeyangkku.domain.letter.domain.LetterEntity
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface LetterRepository : JpaRepository<Letter, Long> {
+interface LetterRepository : JpaRepository<LetterEntity, Long> {
 }
