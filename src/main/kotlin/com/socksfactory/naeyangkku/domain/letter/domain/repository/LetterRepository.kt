e1cc7a86f39fbaefe20f700b074e31fc568d8764
최미래 <miraechoi@dgsw.hs.kr>
2024-11-06 14:20:57 +0900

    (#2) feature :: add find all by owner id

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/domain/repository/LetterRepository.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/domain/repository/LetterRepository.kt
@@ -1,9 +1,11 @@
 package com.socksfactory.naeyangkku.domain.letter.domain.repository
 
 import com.socksfactory.naeyangkku.domain.letter.domain.LetterEntity
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface LetterRepository : JpaRepository<LetterEntity, Long> {
+    fun findAllByOwnerId(workspaceId: Long, pageable: Pageable): List<LetterEntity>
 }
