b4e68b3edf44c1869246ccd0a2b174f739fcb227
최미래 <miraechoi@dgsw.hs.kr>
2024-11-06 14:20:44 +0900

    (#2) feature :: add get letter list service logic

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/service/LetterService.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/service/LetterService.kt
@@ -1,8 +1,11 @@
 package com.socksfactory.naeyangkku.domain.letter.service
 
 import com.socksfactory.naeyangkku.domain.letter.domain.model.Letter
-import com.socksfactory.naeyangkku.domain.letter.presentation.dto.CreateLetterRequest
+import com.socksfactory.naeyangkku.domain.letter.presentation.dto.request.CreateLetterRequest
+import com.socksfactory.naeyangkku.domain.letter.presentation.dto.response.LetterResponse
+import org.springframework.data.domain.Pageable
 
 interface LetterService {
     fun sendLetter(createLetterRequest: CreateLetterRequest): Letter
+    fun getLettersByOwnerId(ownerId: Long, pageable: Pageable): List<LetterResponse>
 }
--- src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/service/LetterServiceImpl.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/service/LetterServiceImpl.kt
@@ -1,19 +1,23 @@
 package com.socksfactory.naeyangkku.domain.letter.service
 
 import com.socksfactory.naeyangkku.domain.letter.domain.LetterEntity
+import com.socksfactory.naeyangkku.domain.letter.domain.mapper.LetterMapper
 import com.socksfactory.naeyangkku.domain.letter.domain.model.Letter
 import com.socksfactory.naeyangkku.domain.letter.domain.repository.LetterRepository
-import com.socksfactory.naeyangkku.domain.letter.presentation.dto.CreateLetterRequest
+import com.socksfactory.naeyangkku.domain.letter.presentation.dto.request.CreateLetterRequest
+import com.socksfactory.naeyangkku.domain.letter.presentation.dto.response.LetterResponse
 import com.socksfactory.naeyangkku.domain.user.domain.UserRepository
 import com.socksfactory.naeyangkku.domain.user.exception.UserErrorCode
 import com.socksfactory.naeyangkku.global.exception.CustomException
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class LetterServiceImpl (
     private val userRepository: UserRepository,
-    private val letterRepository: LetterRepository
+    private val letterRepository: LetterRepository,
+    private val letterMapper: LetterMapper
 ): LetterService {
 
     @Transactional
@@ -29,6 +33,19 @@ class LetterServiceImpl (
 
         val savedEntity = letterRepository.save(letterEntity)
         return savedEntity.toModel()
+    }
+
+    @Transactional
+    override fun getLettersByOwnerId(
+        ownerId: Long,
+        pageable: Pageable
+    ): List<LetterResponse> {
+
+        return letterRepository.findAllByOwnerId(ownerId, pageable).map {
+            letterMapper.transferLetterResponse(
+                letterMapper.toDomain(it)
+            )
+        }
     }
 
     fun LetterEntity.toModel() = Letter(
