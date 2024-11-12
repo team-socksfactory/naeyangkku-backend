520d4adf5e1823a0e7c35605328ef3d88faa41f2
최미래 <miraechoi@dgsw.hs.kr>
2024-11-12 21:50:01 +0900

    (#4) feature :: add get user by username

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/UserRepository.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/UserRepository.kt
@@ -1,9 +1,17 @@
 package com.socksfactory.naeyangkku.domain.user.domain
 
 import com.socksfactory.naeyangkku.domain.user.domain.entity.UserEntity
+import com.socksfactory.naeyangkku.domain.user.exception.UserErrorCode
+import com.socksfactory.naeyangkku.global.exception.CustomException
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository : JpaRepository<UserEntity, Long> {
     fun findByEmail(email: String): UserEntity?
     fun existsByEmail(email: String): Boolean
+    fun findByUsername(username: String): List<UserEntity>
 }
+
+fun UserRepository.getByUsername(username: String): UserEntity =
+    findByUsername(username).firstOrNull() ?: throw CustomException(UserErrorCode.USER_NOT_FOUND)
