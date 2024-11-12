aa60f6a8eaf40237019d2e4a4e3928898e6a116c
최미래 <miraechoi@dgsw.hs.kr>
2024-11-12 22:39:14 +0900

    (#4) fix :: fix oauth properties location

--- src/main/kotlin/com/socksfactory/naeyangkku/global/oauth2/GoogleOAuth2Properties.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/global/oauth2/GoogleOAuth2Properties.kt
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component
 
 @Component
 class GoogleOAuth2Properties(
-    @Value("\${oauth2.google.client-id.web}") val webClientId: String,
-    @Value("\${oauth2.google.client-secret}") val clientSecret: String,
-    @Value("\${oauth2.google.redirect-uri}") val redirectUri: String,
-    @Value("\${oauth2.google.token-uri}") val tokenUri: String,
-    @Value("\${oauth2.google.resource-uri}") val resourceUri: String,
-    @Value("\${oauth2.google.grant-type}") val grantType: String,
+    @Value("\${spring.security.oauth2.google.client-id}") val clientId: String,
+    @Value("\${spring.security.oauth2.google.client-secret}") val clientSecret: String,
+    @Value("\${spring.security.oauth2.google.redirect-uri}") val redirectUri: String,
+    @Value("\${spring.security.oauth2.google.token-uri}") val tokenUri: String,
+    @Value("\${spring.security.oauth2.google.resource-uri}") val resourceUri: String,
+    @Value("\${spring.security.oauth2.google.grant-type}") val grantType: String,
 )
