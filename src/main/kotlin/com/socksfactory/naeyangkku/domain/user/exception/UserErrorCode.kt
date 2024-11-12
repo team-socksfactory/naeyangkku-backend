b3da86532a28a3b27c63fee7589d156486d6fdca
최미래 <miraechoi@dgsw.hs.kr>
2024-11-12 21:49:28 +0900

    (#4) feature :: add user error code

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/user/exception/UserErrorCode.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/user/exception/UserErrorCode.kt
@@ -11,6 +11,9 @@ enum class UserErrorCode (
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND", "유저를 찾을 수 없습니다."),
     USER_ALREADY_EXIST(HttpStatus.CONFLICT, "CONFLICT", "유저가(이메일이) 이미 존재합니다."),
-    USER_NOT_MATCH(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "이메일 또는 비밀번호가 잘못되었습니다.")
+    USER_NOT_MATCH(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "이메일 또는 비밀번호가 잘못되었습니다."),
+    INVAILD_ID_TOKEN(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "유효하지 않은 토큰입니다."),
+    INVAILD_OAUTH_CODE(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", "유효하지 Oauth2 코드 입니다."),
+    INVAILD_PLATFORM_TYPE(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "불가능한 로그인 방식입니다.")
 
 }
