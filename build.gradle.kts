9236951128111675a6279697e886bbada429a812
최미래 <miraechoi@dgsw.hs.kr>
2024-11-12 15:15:23 +0900

    (#4) build :: add oauth dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 }
 
 kotlin {
