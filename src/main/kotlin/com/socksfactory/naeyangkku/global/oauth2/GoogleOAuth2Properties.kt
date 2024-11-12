ab0b5df3052242f471ddf0cb0fa061d051b7137c
최미래 <miraechoi@dgsw.hs.kr>
2024-11-12 21:47:57 +0900

    (#4) feature :: google oauth2 properties

---
+++ src/main/kotlin/com/socksfactory/naeyangkku/global/oauth2/GoogleOAuth2Properties.kt
@@ -0,0 +1,14 @@
+package com.socksfactory.naeyangkku.global.oauth2
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class GoogleOAuth2Properties(
+    @Value("\${oauth2.google.client-id.web}") val webClientId: String,
+    @Value("\${oauth2.google.client-secret}") val clientSecret: String,
+    @Value("\${oauth2.google.redirect-uri}") val redirectUri: String,
+    @Value("\${oauth2.google.token-uri}") val tokenUri: String,
+    @Value("\${oauth2.google.resource-uri}") val resourceUri: String,
+    @Value("\${oauth2.google.grant-type}") val grantType: String,
+)
