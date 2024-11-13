98cc59d4b66fe0a0594ebfbafeb879b203cc50ba
최미래 <miraechoi@dgsw.hs.kr>
2024-11-14 00:50:25 +0900

    (#4) refactor :: remove unuse code

--- src/main/kotlin/com/socksfactory/naeyangkku/global/oauth2/GoogleOAuth2Properties.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/global/oauth2/GoogleOAuth2Properties.kt
@@ -9,6 +9,5 @@ class GoogleOAuth2Properties(
     @Value("\${spring.security.oauth2.google.client-secret}") val clientSecret: String,
     @Value("\${spring.security.oauth2.google.redirect-uri}") val redirectUri: String,
     @Value("\${spring.security.oauth2.google.token-uri}") val tokenUri: String,
-    @Value("\${spring.security.oauth2.google.resource-uri}") val resourceUri: String,
     @Value("\${spring.security.oauth2.google.grant-type}") val grantType: String,
 )
