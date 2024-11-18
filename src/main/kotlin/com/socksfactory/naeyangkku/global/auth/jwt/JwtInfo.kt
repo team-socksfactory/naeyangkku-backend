1c1a9c258b2802cd56fdd523a9217fa11e97c126
최미래 <miraechoi@dgsw.hs.kr>
2024-11-18 11:03:11 +0900

    (#5) fix: fix jwt info response dto

--- src/main/kotlin/com/socksfactory/naeyangkku/global/auth/jwt/JwtInfo.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/global/auth/jwt/JwtInfo.kt
@@ -2,6 +2,10 @@ package com.socksfactory.naeyangkku.global.auth.jwt
 
 data class JwtInfo (
 
+    val id: Long,
+
+    val name: String,
+
     val accessToken: String,
 
     val refreshToken: String
