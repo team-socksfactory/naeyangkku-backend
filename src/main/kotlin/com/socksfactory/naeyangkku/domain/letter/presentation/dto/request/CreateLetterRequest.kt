c6d49ccee7bdbac87146847307684ca8e0e9f13a
최미래 <miraechoi@dgsw.hs.kr>
2024-11-20 20:53:21 +0900

    (#11) refactor: add value to create letter request

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/presentation/dto/request/CreateLetterRequest.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/letter/presentation/dto/request/CreateLetterRequest.kt
@@ -4,5 +4,6 @@ data class CreateLetterRequest(
     val nickname: String = "",
     val content: String = "",
     val iconNm: Long = 0,
-    val ownerId: Long = 0
+    val ownerId: Long = 0,
+    val ownerNickname: String = "",
 )
