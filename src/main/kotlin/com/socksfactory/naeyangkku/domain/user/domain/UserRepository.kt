d8d908a061e94ada1c178f3f1b6fad9c4d485769
최미래 <miraechoi@dgsw.hs.kr>
2024-11-27 14:40:17 +0900

    (#14) feature: add find by nickname

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/UserRepository.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/UserRepository.kt
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface UserRepository : JpaRepository<UserEntity, Long> {
     fun findByEmail(email: String): UserEntity?
     fun existsByEmail(email: String): Boolean
+    fun findByNickname(nickname: String): UserEntity?
 }
