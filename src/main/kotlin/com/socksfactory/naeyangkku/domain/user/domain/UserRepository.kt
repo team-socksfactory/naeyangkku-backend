1c63914352520a98536aa0c7715560d0482db9b9
최미래 <miraechoi@dgsw.hs.kr>
2024-12-10 16:44:41 +0900

    (#4) feature: add find user by email

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/UserRepository.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/UserRepository.kt
@@ -2,9 +2,11 @@ package com.socksfactory.naeyangkku.domain.user.domain
 
 import com.socksfactory.naeyangkku.domain.user.domain.entity.UserEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface UserRepository : JpaRepository<UserEntity, Long> {
     fun findByEmail(email: String): UserEntity?
+    fun findUserByEmail(email: String): Optional<UserEntity>
     fun findByNickname(nickname: String): UserEntity?
     fun existsByEmail(email: String): Boolean
     fun existsByNickname(nickname: String): Boolean
