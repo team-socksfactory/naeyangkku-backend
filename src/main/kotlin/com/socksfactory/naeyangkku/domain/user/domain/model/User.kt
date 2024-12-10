64f5ae94246c8434de01064f27adcc5e0c560851
최미래 <miraechoi@dgsw.hs.kr>
2024-12-10 16:46:12 +0900

    (#4) feature: add companion object to user domain model

--- src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/model/User.kt
+++ src/main/kotlin/com/socksfactory/naeyangkku/domain/user/domain/model/User.kt
@@ -1,6 +1,8 @@
 package com.socksfactory.naeyangkku.domain.user.domain.model
 
+import com.socksfactory.naeyangkku.domain.user.domain.entity.UserEntity
 import com.socksfactory.naeyangkku.domain.user.domain.enums.UserRoles
+import org.springframework.stereotype.Component
 
 data class User(
     val id: Long? = null,
@@ -8,4 +10,27 @@ data class User(
     val nickname: String = "",
     val password: String = "",
     val role: UserRoles = UserRoles.ROLE_USER
-)
+) {
+
+    @Component
+    companion object{
+        fun toUser(userEntity: UserEntity): User {
+            return User(
+                email = userEntity.email,
+                nickname = userEntity.nickname,
+                password = userEntity.password,
+                role = userEntity.role,
+            )
+        }
+
+        fun toEntity(user: User): UserEntity {
+            return UserEntity(
+                email = user.email,
+                nickname = user.nickname,
+                password = user.password,
+                role = user.role
+            )
+        }
+    }
+
+}
